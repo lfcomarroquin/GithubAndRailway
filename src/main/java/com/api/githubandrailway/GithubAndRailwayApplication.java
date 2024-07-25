8f7e7ae8af70849fa91e79e435e4ec07598ed10a
Luis Francisco Marroquin Ortiz <lfortiz.agt@esieduc.org>
2024-07-25 14:57:07 -0600

    Despliegue App Spring Boot con Github y Raylway.app

---
+++ src/main/java/com/api/githubandrailway/GithubAndRailwayApplication.java
@@ -0,0 +1,13 @@
+package com.api.githubandrailway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubAndRailwayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubAndRailwayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/api/githubandrailway/GithubAndRailwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.githubandrailway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubAndRailwayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
